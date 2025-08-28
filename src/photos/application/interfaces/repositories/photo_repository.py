b340ab3e9df1f6be4fa970b819bf7318a22f83fa
MDx3R <nationmdxr@gmail.com>
2025-08-28 18:53:49 +0300

    update interface to specify presigned url type and add method to get many presigned urls

--- src/photos/application/interfaces/repositories/photo_repository.py
+++ src/photos/application/interfaces/repositories/photo_repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections.abc import Sequence
 from datetime import timedelta
 from typing import BinaryIO
 
@@ -9,6 +10,10 @@ class IPhotoRepository(ABC):
         self, name: str, mime: str, data: BinaryIO
     ) -> None: ...
     @abstractmethod
-    async def get_presigned_url(
+    async def get_presigned_get_url(
         self, name: str, expires_in: timedelta
     ) -> str: ...
+    @abstractmethod
+    async def get_presigned_get_urls(
+        self, names: Sequence[str], expires_in: timedelta
+    ) -> list[str]: ...
