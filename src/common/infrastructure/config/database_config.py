79cbcd651f67fe278132230e59b6b9fdedfd12a0
MDx3R <nationmdxr@gmail.com>
2025-08-29 15:12:09 +0300

    rename db_password to db_pass

--- src/common/infrastructure/config/database_config.py
+++ src/common/infrastructure/config/database_config.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 class DatabaseConfig(BaseModel):
     db_name: str | None = None
     db_user: str | None = None
-    db_password: str | None = None
+    db_pass: str | None = None
     db_host: str | None = None
     db_port: int | None = None
 
     @property
     def database_url(self) -> str:
         return (
-            f"postgresql+asyncpg://{self.db_user}:{self.db_password}"
+            f"postgresql+asyncpg://{self.db_user}:{self.db_pass}"
             f"@{self.db_host}:{self.db_port}/{self.db_name}"
         )
