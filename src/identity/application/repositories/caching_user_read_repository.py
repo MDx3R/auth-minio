a65c82ce67e6f5048e14b6609cf4954db9157588
MDx3R <nationmdxr@gmail.com>
2025-08-30 16:24:46 +0300

    introduce key value cache and implement caching user read repo

---
+++ src/identity/application/repositories/caching_user_read_repository.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from common.application.repositories.key_value_cache import KeyValueCache
+from identity.application.interfaces.repositories.user_read_repository import (
+    IUserReadRepository,
+)
+from identity.application.read_models.user_read_model import UserReadModel
+
+
+class CachingUserReadRepository(IUserReadRepository):
+    def __init__(
+        self,
+        user_read_repository: IUserReadRepository,
+        key_value_cache: KeyValueCache[UserReadModel],
+    ) -> None:
+        self.user_read_repository = user_read_repository
+        self.key_value_cache = key_value_cache
+
+    async def get_by_id(self, user_id: UUID) -> UserReadModel:
+        key = self.key_value_cache.make_key(str(user_id))
+
+        cached = await self.key_value_cache.get(key)
+        if cached:
+            return cached
+
+        user = await self.user_read_repository.get_by_id(user_id)
+
+        await self.key_value_cache.set(key, user)
+        return user
---
+++ tests/unit/identity/application/repositories/test_caching_user_read_repository.py
@@ -0,0 +1,60 @@
+from unittest.mock import Mock
+from uuid import uuid4
+
+import pytest
+
+from common.application.repositories.key_value_cache import KeyValueCache
+from identity.application.interfaces.repositories.user_read_repository import (
+    IUserReadRepository,
+)
+from identity.application.read_models.user_read_model import UserReadModel
+from identity.application.repositories.caching_user_read_repository import (
+    CachingUserReadRepository,
+)
+
+
+@pytest.mark.asyncio
+class TestCachingUserReadModelRepository:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.user_read_repository = Mock(spec=IUserReadRepository)
+        self.key_value_cache = Mock(spec=KeyValueCache)
+        self.ttl = 300
+
+        self.repository = CachingUserReadRepository(
+            self.user_read_repository, self.key_value_cache
+        )
+
+        self.user_id = uuid4()
+        self.user = Mock(spec=UserReadModel)
+
+        self.key = f"{self.user_id}"
+        self.key_value_cache.make_key.return_value = self.key
+
+    async def test_get_by_id_cache_hit(self):
+        # Arrange
+        self.key_value_cache.get.return_value = self.user
+
+        # Act
+        result = await self.repository.get_by_id(self.user_id)
+
+        # Assert
+        assert result == self.user
+        self.key_value_cache.get.assert_awaited_once_with(self.key)
+        self.user_read_repository.get_by_id.assert_not_called()
+
+    async def test_get_by_id_cache_miss_fetches_and_caches(self):
+        # Arrange
+        self.key_value_cache.get.return_value = None
+        self.user_read_repository.get_by_id.return_value = self.user
+
+        # Act
+        result = await self.repository.get_by_id(self.user_id)
+
+        # Assert
+        assert result == self.user
+        self.key_value_cache.get.assert_awaited_once_with(self.key)
+        self.user_read_repository.get_by_id.assert_awaited_once_with(
+            self.user_id
+        )
+        self.key_value_cache.set.assert_awaited_once_with(self.key, self.user)
